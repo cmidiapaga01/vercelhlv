38743b3049baa21b9d6472383564077c02b5464d
cmidiapaga01 <124449911+cmidiapaga01@users.noreply.github.com>
2024-03-26 13:24:31 +0100

    ma oddfdd

--- src/components/SimpleCookiePreference.js
+++ src/components/SimpleCookiePreference.js
@@ -58,11 +58,7 @@ export default function SimpleCookiePreference() {
 
       <Stack direction={{ base: 'column', md: 'row' }} justifyContent="space-between">
         <Text fontSize={{ base: 'sm' }} textAlign={'left'} maxW={'4xl'}>
-          We use cookies and similar technologies to help personalise content, tailor and
-          measure ads, and provide a better experience. By clicking OK or turning an
-          option on in Cookie Preferences, you agree to this, as outlined in our Cookie
-          Policy. To change preferences or withdraw consent, please update your Cookie
-          Preferences.
+          
         </Text>
         <Stack direction={{ base: 'column', md: 'row' }}>
           {!cookiePreferencesClicked && (
